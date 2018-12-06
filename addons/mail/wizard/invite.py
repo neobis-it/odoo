f122a8e3c4950e40ce4f122256cef465927b2917
Stephan Rozendaal <stephan@west.nl>
2018-12-06 11:51:49 +0100

    Revert "[FIX] mail: invite: use proper translation terms"
    This reverts commit 1630581549a49b0d152a6b5ebc3693fae854c231.

--- addons/mail/wizard/invite.py
+++ addons/mail/wizard/invite.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from lxml import etree
-from lxml.html import builder as html
-
 from odoo import _, api, fields, models
 
 
@@ -15,27 +12,18 @@ class Invite(models.TransientModel):
     @api.model
     def default_get(self, fields):
         result = super(Invite, self).default_get(fields)
-        if self._context.get('mail_invite_follower_channel_only'):
-            result['send_mail'] = False
-        if 'message' not in fields:
-            return result
-
         user_name = self.env.user.name_get()[0][1]
         model = result.get('res_model')
         res_id = result.get('res_id')
-        if model and res_id:
-            document = self.env['ir.model'].search([('model', '=', model)]).name_get()[0][1]
-            title = self.env[model].browse(res_id).display_name
-            msg_fmt = _('%(user_name)s invited you to follow %(document)s document: %(title)s')
-        else:
-            msg_fmt = _('%(user_name)s invited you to follow a new document')
-
-        text = msg_fmt % locals()
-        message = html.DIV(
-            html.P(_('Hello,')),
-            html.P(text)
-        )
-        result['message'] = etree.tostring(message)
+        if self._context.get('mail_invite_follower_channel_only'):
+            result['send_mail'] = False
+        if 'message' in fields and model and res_id:
+            model_name = self.env['ir.model']._get(model).display_name
+            document_name = self.env[model].browse(res_id).name_get()[0][1]
+            message = _('<div><p>Hello,</p><p>%s invited you to follow %s document: %s.</p></div>') % (user_name, model_name, document_name)
+            result['message'] = message
+        elif 'message' in fields:
+            result['message'] = _('<div><p>Hello,</p><p>%s invited you to follow a new document.</p></div>') % user_name
         return result
 
     res_model = fields.Char('Related Document Model', required=True, index=True, help='Model of the followed resource')
@@ -58,8 +46,7 @@ class Invite(models.TransientModel):
             new_channels = wizard.channel_ids - document.message_channel_ids
             document.message_subscribe(new_partners.ids, new_channels.ids)
 
-            model_ids = self.env['ir.model'].search([('model', '=', wizard.res_model)])
-            model_name = model_ids.name_get()[0][1]
+            model_name = self.env['ir.model']._get(wizard.res_model).display_name
             # send an email if option checked and if a message exists (do not send void emails)
             if wizard.send_mail and wizard.message and not wizard.message == '<br>':  # when deleting the message, cleditor keeps a <br>
                 message = self.env['mail.message'].create({
